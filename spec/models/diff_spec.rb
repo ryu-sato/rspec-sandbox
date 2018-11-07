d012d8cdfe4cae8e3b6828515ebccd25eac0f940
Ryu Sato <ryu@weseek.co.jp>
2018-11-08 01:48:38 +0900

    model の valid 判定を be_valid マッチャを使うよう修正

--- spec/models/diff_spec.rb
+++ spec/models/diff_spec.rb
@@ -14,6 +14,6 @@ require 'rails_helper'
 RSpec.describe Diff, type: :model do
   it "is valid with diff string" do
     diff = Diff.new(diff: "some diff")
-    expect(diff.valid?).to be_truthy
+    expect(diff).to be_valid
   end
 end
