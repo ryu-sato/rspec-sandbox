4432a6a000861f3291117d74431a8f5989beea23
Ryu Sato <ryu@weseek.co.jp>
2018-11-08 03:31:57 +0900

    projectモデルのvalidationに対するテストを追加

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -4,6 +4,24 @@ RSpec.describe Project, type: :model do
   let(:user)  { User.create(first_name: "John", last_name: "Doe", email: "john@a.a", password: "password") }
   let(:other) { User.create(first_name: "Bob",  last_name: "Doe", email: "bob@a.a",  password: "password") }
 
+  # 名前、関連ユーザがあれば有効な状態であること
+  it "is valid with a name and referenced with user" do
+    project = user.projects.build(name: "Some Project Name")
+    expect(project).to be_valid
+  end
+
+  # 名前がなければ無効な状態であること
+  it "is invalid without a name" do
+    project = user.projects.build(name: nil)
+    expect(project).to be_invalid
+  end
+
+  # 関連ユーザがなければ無効な状態であること
+  it "is invalid without a name" do
+    project = Project.new(name: "Some Project Name")
+    expect(project).to be_invalid
+  end
+
   # ユーザー単位では重複したプロジェクト名を許可しないこと
   it "does not allow duplicate project names per user" do
     same_project_name = 'Same Project Name'
